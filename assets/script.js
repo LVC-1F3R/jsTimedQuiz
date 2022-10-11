9114a0ad9f034870c03bd555bf91a0867d39e848
iskry <refocuslucifer@protonmail.com>
2022-10-11 09:04:27 -0500

    added questions. added hide and display logic for questions. added partial incorrect answer flow

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,19 @@
-var isSetTimmeoutRunning = false;
-  
-function startCountdown(){
+// refractor code 
 
-  if( isSetTimmeoutRunning == false ){
+function handleStart() {
+var isSetTimeoutRunning = false;
+  
+  if( isSetTimeoutRunning == false ){
    
-    var timeCount = 60
+    var timeCount = 120
 
     if( timeCount >=1 ){
-      isSetTimmeoutRunning = true;
+      isSetTimeoutRunning = true;
       var countMilliseconds = timeCount*1000;
       setTimeout(function(){
-        isSetTimmeoutRunning = false;
-        alert(timeCount + " seconds have passed.");
+        isSetTimeoutRunning = false;
+        alert(" Time is up!");
+        // redirect to scoreboard
       }, countMilliseconds);
     
       var counter = timeCount;
@@ -28,10 +30,51 @@ function startCountdown(){
       }, 1000);
     }
   }
+
+
+if (isSetTimeoutRunning == true) {
+  var startButton = document.querySelector("#start-button")
+  startButton.style.display = "none"
+
+  var firstQuestion = document.querySelector("#first-question")
+  firstQuestion.style.display = "block"
+
+  var secondQuestion = document.querySelector("#second-question")
+  var thirdQuestion = document.querySelector("#third-question")
+
+  document.getElementById("correct1").onclick = function() {
+    firstQuestion.style.display = "none"
+    secondQuestion.style.display = "block"
+  }
+
+  document.getElementById("correct2").onclick = function() {
+    secondQuestion.style.display = "none"
+    thirdQuestion.style.display = "block"
+  }
+
+  document.getElementById("correct3").onclick = function() {
+    thirdQuestion.style.display = "none"
+    // show scoreboard and stop timer
+  }
+
+
+// handler for incorrect answers along with time and point reduction. 
+
+  var incorrectAns = document.querySelectorAll("button#incorrect")
+  console.log(incorrectAns)
+  for (const incorrect of incorrectAns) {
+    incorrect.addEventListener('click',  function(event) {
+    alert("Incorrect")
+    // subtract time
+    // subtract score
+  })
 }
 
 
-function handleStart() {
+} else {
+  // show scoreboard
+}
+
 
 
 
